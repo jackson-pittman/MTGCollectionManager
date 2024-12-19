084f283dd828fad8ba54b368629c2e28cb0d1506
Jackson <jcp3236@mavs.uta.edu>
2024-12-19 00:38:54 -0600

    Setup complete

---
+++ src/test/java/com/mtg/collection/MtgCollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtg.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgCollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
